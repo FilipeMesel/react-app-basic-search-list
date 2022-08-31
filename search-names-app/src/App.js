5c0c79fceb1dc48e8a9cf5359855cac3169415e9
FilipeMesel <lipemesel@hotmail.com>
2022-08-30 23:31:12 -0300

    adding component search

--- search-names-app/src/App.js
+++ search-names-app/src/App.js
@@ -2,33 +2,13 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import JASONDATA from './MOCK_DATA.json'
-
+import Search from './components/Search'
 function App() {
-  const [textInput, setTextInput] = useState('')
   return (
-    <div className="App">
-      <input 
-          type = "text"
-          placeholder='Search...'
-          onChange = {(event) => {
-          setTextInput(event.target.value);
-          }}
-          />
-        {JASONDATA.filter((val)=>{
-          if(textInput == "") {
-            return val
-          } else if(val.first_name.toLowerCase().includes(textInput.toLowerCase())) {
-            return val
-          }
-        }).map((val, key) => {
-          return (
-            <div  className="user" key = {key}>
-              <p>{val.first_name}</p>
-            </div>
-          )
-        })}
+    <div>
+      <Search />
     </div>
-  );
+  )
 }
 
 export default App;
